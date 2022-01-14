4371372aa258d353b112174a2897d9b79d6b539b
Puspita Vabna <sharif7761@gmail.com>
2022-01-15 00:46:24 +0600

    post edit page design

---
+++ resources/views/admin/post/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('admin.layouts.admin_layout')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Edit Post</h5>
+            <form>
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control" id="title" value="{{ old('title', $post->title) }}">
+                </div>
+                <div class="form-group">
+                    <label for="subtitle">Subtitle</label>
+                    <input type="text" name="subtitle" class="form-control" id="subtitle" value="{{ old('subtitle', $post->subtitle) }}">
+                </div>
+                <div class="form-group">
+                    <label for="body">Body</label>
+                    <textarea class="form-control" name="body" id="body" >value="{{ old('body', $post->body) }}"</textarea>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image</label>
+                    <input type="file" class="form-control" name="image" id="image">
+                </div>
+                <div class="form-group">
+                    <label for="status">Publish</label>
+                    <input type="checkbox" name="status" id="status">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
