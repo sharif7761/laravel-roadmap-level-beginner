93aaecf0c362cc4a952fd50d8fae202dd50698a7
Puspita Vabna <sharif7761@gmail.com>
2022-01-17 00:37:35 +0600

    tag add modal

--- resources/views/admin/tag/index.blade.php
+++ resources/views/admin/tag/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+    <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+        Add Tag
+    </button>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -28,5 +31,29 @@
             </tr>
             @endforeach --}}
         </tbody>
+    </table>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Tag</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" id="name" value="{{ old('name') }}">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
